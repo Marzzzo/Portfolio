bf7c91b559dd356120b541d429739f2f5f20c6c4
Marzzzo <marco.burdick@web.de>
2026-06-11 20:29:25 +0200

    edit the skill-section icons,the scroll-line in herosection and animation from the buttons

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// switch language German and English
 function setLanguage(language) {
   const btnEN = document.getElementById('btnEN');
   const btnDE = document.getElementById('btnDE');
@@ -12,12 +13,14 @@ function setLanguage(language) {
 
 let images = document.querySelectorAll('.img-container img');
 
+//hide Image Exit hover in Projects
 function hideImages() {
   images.forEach((image) => {
     image.style.display = 'none';
   });
 }
 
+//show Image by hover in Projects
 function showImage(className) {
   hideImages();
   const image = document.querySelector(`.${className}`);

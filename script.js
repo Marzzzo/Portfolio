3e5ab9a0882350b2de8dac5316416aa2f35c76d4
Marzzzo <marco.burdick@web.de>
2026-08-18 17:50:12 +0200

    added scroll animation

--- script.js
+++ script.js
@@ -394,3 +394,20 @@ function initNavigation() {
     }
   });
 }
+
+const scrollElements = document.querySelectorAll(".scroll-animation");
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+scrollElements.forEach((element) => {
+  observer.observe(element);
+});
